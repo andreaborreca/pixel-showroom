42a4320717649e7246efc3e929bb3a183f241f0c
Andrea Borreca <72610350+andreaborreca@users.noreply.github.com>
2024-09-14 11:51:19 +0200

    new piano version

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 //piano
 
-var playNote = function() {
+var playNote = function () {
     var sound = this.getAttribute("data-sound");
     var note = new Audio(sound);
     note.play();
 };
 
 var keys = document.getElementsByClassName("key");
+var dkeys = document.getElementsByClassName("dkey");
 
 for (var i = 0; i < keys.length; i++) {
     keys[i].addEventListener('click', playNote);
+}
+
+for (var i = 0; i < dkeys.length; i++) {
+    dkeys[i].addEventListener('click', playNote);
 }
 
 //gioconda
@@ -21,14 +26,14 @@ const frame = document.getElementById("frame");
 function elementsOverlap(el1, el2) {
     const domRect1 = el1.getBoundingClientRect();
     const domRect2 = el2.getBoundingClientRect();
-  
+
     return !(
-      domRect1.top > domRect2.bottom ||
-      domRect1.right < domRect2.left ||
-      domRect1.bottom < domRect2.top ||
-      domRect1.left > domRect2.right
+        domRect1.top > domRect2.bottom ||
+        domRect1.right < domRect2.left ||
+        domRect1.bottom < domRect2.top ||
+        domRect1.left > domRect2.right
     );
-  }
+}
 
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -58,24 +63,24 @@ function dragElement(elmnt) {
         elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
 
-        if(!(elementsOverlap(gioconda, sunglasses))){
+        if (!(elementsOverlap(gioconda, sunglasses))) {
             elmnt.style = "";
         }
 
-        if(elementsOverlap(frame, sunglasses)){
+        if (elementsOverlap(frame, sunglasses)) {
             elmnt.style.top = "41%";
             elmnt.style.left = "50%";
             elmnt.style.cursor = "auto";
             elmnt.style.pointerEvents = "none";
             y = 0;
-            myIntervalTimer = setInterval(function(){
-                if(y === 3 || y === 6){
+            myIntervalTimer = setInterval(function () {
+                if (y === 3 || y === 6) {
                     elmnt.style.top = "42%";
                 }
-                if ((y === 6 || y === 9) ) {
+                if ((y === 6 || y === 9)) {
                     elmnt.style.top = "43%";
                 }
-                if(y === 10){
+                if (y === 10) {
                     elmnt.style.top = "44%";
                 }
                 y++;
